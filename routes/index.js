e144414ce9d7447fa6dc914cae91095c7c4103d3
Eduardo Jirón <emjironal@gmail.com>
2019-04-17 17:02:42 -0600

    Empiezo del backoffice

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { Client } = require('pg');
+var bluebird = require('bluebird');
+var pgp = require('pg-promise')({
+    promiseLib: bluebird
+});
 const connectionUrl = 'postgres://rcygnhpuisrxld:9b08ad521466ea78f8697fe956e534d9a300d52f0d164f67f4eb0fe80833698f@ec2-54-225-95-183.compute-1.amazonaws.com:5432/d1rqbmoplu7o3p?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
-/*const client = new Client(
-{
-  user: 'rcygnhpuisrxld',
-  host: 'ec2-54-225-95-183.compute-1.amazonaws.com',
-  database: 'd1rqbmoplu7o3p',
-  password: '9b08ad521466ea78f8697fe956e534d9a300d52f0d164f67f4eb0fe80833698f',
-  port: 5432
-});*/
-const client = new Client(
-  {
-    connectionString: connectionUrl
-  }
-);
+var db = pgp(connectionUrl);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next)
@@ -22,14 +14,30 @@ router.get('/', function(req, res, next)
   res.render('index', { title: 'Appetyte backoffice' });
 });
 
-router.post('/prueba', function(req, res, next)
+router.post('/login', function(req, res, next)
 {
-  res.render('index', {title: 'Prueba sirve'});
-  client.connect();
-  client.query('SELECT * from provincia', (err, res) => {
-    console.log(err, res);
-    client.end();
-  });
+  res.redirect('/');
+  var username = req.body.txtUsuario;
+  var password = req.body.txtPassword;
+  db.any('select * from usertable where type = \'administrador\' and ' +
+    'username = \'' + username + '\' and password = \'' + password +'\'')
+  .then(result =>
+    {
+      if(result.length < 1)
+      {
+        console.log('Result:', 'vacio');
+      }
+      else
+      {
+        console.log('Result:', result[0]);
+      }
+    }
+  )
+  .catch(err =>
+    {
+    console.log('Error: ', err);
+    }
+  );
 });
 
 module.exports = router;
