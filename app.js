e549df0a7159fa1025a0711525850949d93c2b4e
Eduardo Jirón <emjironal@gmail.com>
2019-04-20 16:19:50 -0600

    Funciones para app y cambio de modificar codigo distrito para restaurant

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var administradorRouter = require('./routes/administrador');
 var insertarRouter = require('./routes/insertarRestaurante');
+var androidRouter = require('./routes/android');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/administrador', administradorRouter);
 app.use('/insertarRestaurante', insertarRouter);
+app.use('/android', androidRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
